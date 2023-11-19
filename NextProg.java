c2ef79b74c92af218fc0a552141bd6333ddfa620
ARUNKUMAR RAVICHANDRAN <arunrjsa@gmail.com>
2023-11-19 14:36:18 +0530

    done a program Nextp, ol, switch

---
+++ NextProg.java
@@ -0,0 +1,8 @@
+class NextProg
+{
+	public static void main(String args[]){
+  	  double pi=3.1416,r=10.8,a;
+  	  a=pi*r*r;
+  	 System.out.println("Area of circle is "+ a);
+	}
+}
---
+++ Overloading.java
@@ -0,0 +1,1 @@
+class Overloading{ void x(int i) { System.out.println("one"); } void x(String s){ System.out.println("two"); } void x(double d) { System.out.println("three"); } public static void main(String[] args) { new Overloading () .x (4.0); } }
